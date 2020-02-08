b2db6f765af5d956dede2c224bb58cda7fbc30bb
Khalil <khalilhall1221@gmail.com>
2020-02-08 15:56:52 -0500

    set start questions

--- server.js
+++ server.js
@@ -15,13 +15,36 @@ var connection = mysql.createConnection({
 for some reason when the if statement isnt commented out it breaks code
 */
 connection.connect(function(err){
-    console.log("connected");
-    // if (err) throw err;
+    // if(err){
+    //     console.log(err);
+    // }else{
+        console.log("connected as id " + connection.threadId);
+    // // if (err) throw err;
     
-    // console.log("connected as id " + connection.threadId);
-    afterConnection();
+    // // console.log("connected as id " + connection.threadId);
+    // afterConnection();
+    // }
+    start();
 });
+function start(){
+    inquirer.prompt([
+       {
+           type: "list",
+           name: "choice",
+           message: "What would you like to do?",
+           choices: [
+               "Add an employee?",
+               "Add a role?",
+               "Add a department?",
+               "Update an employees Role?",
+               "Delete an employee?"
+           ] 
+       }
+    ])
+    // .then(function(answer){
 
+    // })
+}
 function afterConnection(){
     connection.query("SELECT * FROM department", function(err, res){
         // if(err) throw err;
